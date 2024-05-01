a743a5c768fd6bf483c7bd6da01dcd82b31833ef
pizzuhh <pizuhh@gmail.com>
2024-05-01 17:42:24 +0300

    3.2 - added update checker

---
+++ src/config.h
@@ -0,0 +1,8 @@
+#pragma once
+/*
+This file will be used to easily configure versions, authors, etc.
+*/
+
+#define AUTHOR "pizzuhh"
+#define RELEASE_URL "https://api.github.com/repos/pizzuhh/pizzeria/releases"
+#define VERSION "3.2"
