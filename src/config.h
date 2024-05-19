79fb90fd68200730dfad1aaa24cbc2a70bc7a1ef
pizzuhh <admin@pizzuhh.dev>
2024-05-19 18:24:11 +0300

    patch

--- src/config.h
+++ src/config.h
@@ -7,7 +7,7 @@ This file will be used to easily configure versions, authors, etc.
 
 #define AUTHOR "pizzuhh"
 #define RELEASE_URL "https://api.github.com/repos/pizzuhh/pizzeria/releases"
-#define VERSION "3.4.1"
+#define VERSION "3.4.2"
 #define DISABLE_UPDATE_CHECK
 
 
