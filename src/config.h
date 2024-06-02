7bfe397860789c121be39134a55bec1876900d91
pizzuhh <admin@pizzuhh.dev>
2024-06-02 21:06:41 +0300

    config.h

--- src/config.h
+++ src/config.h
@@ -9,5 +9,5 @@ This file will be used to easily configure versions, authors, etc.
 #define RELEASE_URL "https://api.github.com/repos/pizzuhh/pizzeria/releases"
 #define VERSION "3.4.3"
 #define LOG_MESSAGES
-//#define DISABLE_UPDATE_CHECK
+#define DISABLE_UPDATE_CHECK
 
